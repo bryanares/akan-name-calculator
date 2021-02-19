39e1faf4935008b7638a1efda96ef0fd74b25cc1
bryanares <bryanares12@gmail.com>
2021-02-19 13:10:55 +0300

    added arrays for both male and female akan names. also added the days of the week array

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-const akanForm = document.querySelector('#akanForm');
-const outputContainer = document.querySelector('#output');
+const akanForm = document.querySelector("#akanForm");
+const outputContainer = document.querySelector("#output");
 const genderValue = ()=>{
-    if(document.getElementById('male').selected){
+    if(document.getElementById("male").selected){
         return document.getElementById('male').value;
     }
-    if(document.getElementById('female')).selected{
-        return document.getElementById('female').value;
+    if(document.getElementById("female")).selected{
+        return document.getElementById("female").value;
     }
     return false;
 };
+//form manipulation
+
+akanForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    const maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const femaleAkanNames =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    
+})
